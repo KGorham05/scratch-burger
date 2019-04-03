846d7cc468c98bad3b4db8841d5811ef1069a565
Kgor <kevin.gorham@gmail.com>
2019-04-02 20:37:31 -0700

    starter code added

---
+++ config/orm.js
@@ -0,0 +1,26 @@
+const connection = require("./connection");
+
+const orm = {
+    findAll: function(tableName, cb) {
+        connection.query("SELECT * FROM ??", [tableName], (err, results) => {
+            if (err) throw err;
+            cb(results);
+        })
+    },
+    create: function(tableName, recordAsObject, cb) {
+        connection.query("INSERT INTO ?? SET ?", [tableName, recordAsObject], (err, results) => {
+            if (err) throw err;
+            cb(results);
+        })       
+    },
+    
+};
+
+orm.findAll("burgers", (data)=> console.table(data));
+
+const myBurg = {
+    name: "Chicken Bacon Ranch"
+}
+
+orm.create("burgers", myBurg, (data) => console.log(data));
+module.exports = orm;
--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log(true);
+const express = require("express");
+const app = express();
+const exphbs  = require('express-handlebars');
+const orm = require('./config/orm')
+
+const PORT = process.env.PORT || 8080;
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.render("burgers");
+})
+
+app.listen(PORT, () => console.log(`App is running on http://localhost:${PORT}`))
